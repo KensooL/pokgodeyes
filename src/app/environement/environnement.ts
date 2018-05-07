c445cd92f78377347aa05dc3a7c268fad7ec0b30
Prince ond <ondonda.prince@hotmail.fr>
2018-05-07 15:28:51 +0200

    first commit

---
+++ src/app/environement/environnement.ts
@@ -0,0 +1,11 @@
+export const environement = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAxmvfTh1pRhI_w0V1Pra7qfeHhuQpkmKI",
+    authDomain: "newagent-5e5d4.firebaseapp.com",
+    databaseURL: "https://newagent-5e5d4.firebaseio.com",
+    projectId: "newagent-5e5d4",
+    storageBucket: "newagent-5e5d4.appspot.com",
+    messagingSenderId: "973825031927"
+  }
+};
